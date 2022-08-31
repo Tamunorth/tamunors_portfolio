6add1b16e2e017537fbfe55fcb0dc40aa24c5bd3
favour davies-manuel <daviesmanuelfavour@gmail.com>
2022-08-31 11:30:33 +0100

    Mobile UI updates

--- lib/screens/mobile/mobile_body.dart
+++ lib/screens/mobile/mobile_body.dart
@@ -4,28 +4,162 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:tams_portfolio/_lib.dart';
 
-class MobileBody extends StatelessWidget {
+class MobileBody extends StatefulWidget {
   MobileBody({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<MobileBody> createState() => _MobileBodyState();
+}
+
+class _MobileBodyState extends State<MobileBody> {
+  final widgetsList = [
+    Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: ImageSlideShow(imageList: [
+        Assets.pngsDropie1,
+        Assets.pngsDropie2,
+        Assets.pngsDropie3,
+        Assets.pngsDropie4,
+      ]),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: ImageSlideShow(imageList: [
+        Assets.pngsTx1,
+        Assets.pngsTx2,
+        Assets.pngsTx3,
+        Assets.pngsTx4,
+      ]),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: ImageSlideShow(imageList: [
+        Assets.pngsHollow1,
+        Assets.pngsHollow2,
+        Assets.pngsHollow3,
+        Assets.pngsHollow4,
+      ]),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: ImageSlideShow(imageList: [
+        Assets.pngsMedbury1,
+        Assets.pngsMedbury2,
+        Assets.pngsMedbury3,
+        Assets.pngsMedbury4,
+      ]),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: ImageSlideShow(imageList: [
+        Assets.pngsTitan1,
+        Assets.pngsTitan2,
+        Assets.pngsTitan3,
+        Assets.pngsTitan4,
+      ]),
+    ),
+  ];
+
   final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    scrollController.addListener(() {
+      if (scrollController.hasClients) {}
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // WidgetsBinding.instance.addPostFrameCallback((_) =>
+    //     scrollController.animateTo(scrollController.position.maxScrollExtent,
+    //         duration: Duration(seconds: 3), curve: Curves.decelerate));
+
     return Scaffold(
       backgroundColor: Colors.black,
+      endDrawer: Drawer(
+        backgroundColor: Pallets.bgColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              margin: EdgeInsets.symmetric(vertical: 20.0),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Color(0x40FFF9EA),
+                  Color(0x1AFFFFFF),
+                ]),
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                  color: Color(0xCCFFFFFF),
+                ),
+              ),
+              child: AppBarText(
+                text: 'Projects',
+                url: AppStrings.githubReposUrl,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Color(0x40FFF9EA),
+                  Color(0x1AFFFFFF),
+                ]),
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                  color: Color(0xCCFFFFFF),
+                ),
+              ),
+              child: AppBarText(
+                text: 'Github',
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              margin: EdgeInsets.symmetric(vertical: 20.0),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Color(0x40FFF9EA),
+                  Color(0x1AFFFFFF),
+                ]),
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                  color: Color(0xCCFFFFFF),
+                ),
+              ),
+              child: AppBarText(
+                text: 'Resume',
+                url: AppStrings.resumeUrl,
+              ),
+            ),
+          ],
+        ),
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            VGap(height: window.physicalSize.height * 0.1),
+            VGap(height: window.physicalSize.height * 0.05),
             Container(
+              padding: EdgeInsets.all(window.physicalSize.width * 0.05),
               margin: EdgeInsets.symmetric(
-                  horizontal: window.physicalSize.width * 0.05),
+                  horizontal: window.physicalSize.width * 0.02),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   CustomAppBar(),
-                  VGap(height: window.physicalSize.height * 0.05),
+                  VGap(height: window.physicalSize.height * 0.02),
                   Container(
                     width: window.physicalSize.width,
                     child: Column(
@@ -41,16 +175,11 @@ class MobileBody extends StatelessWidget {
                                 fontFamily: AppStrings.conthraxFont,
                               ),
                             ),
-                            window.physicalSize.width > 1000
-                                ? Container(
-                                    height: 23.0,
-                                    color: Pallets.accentColor,
-                                    width: 400)
-                                : Container(
-                                    height: 10.0,
-                                    width: 50,
-                                    color: Pallets.accentColor,
-                                  ),
+                            Container(
+                              height: 10.0,
+                              width: 50,
+                              color: Pallets.accentColor,
+                            ),
                           ],
                         ),
                         const GradientText(
@@ -61,7 +190,7 @@ class MobileBody extends StatelessWidget {
                             fontFamily: AppStrings.conthraxFont,
                           ),
                         ),
-                        VGap(height: window.physicalSize.height * 0.027),
+                        VGap(height: window.physicalSize.height * 0.02),
                         Text(
                           AppStrings.description,
                           style: TextStyle(
@@ -76,9 +205,9 @@ class MobileBody extends StatelessWidget {
                             width: 250,
                           ),
                         ),
-                        VGap(height: window.physicalSize.height * 0.05),
+                        VGap(height: window.physicalSize.height * 0.02),
                         GradientButton(),
-                        VGap(height: window.physicalSize.height * 0.08),
+                        VGap(height: window.physicalSize.height * 0.02),
                       ],
                     ),
                   ),
@@ -159,7 +288,7 @@ class MobileBody extends StatelessWidget {
               gradient: Pallets.appGradient,
               textAlign: TextAlign.center,
               style: TextStyle(
-                fontSize: 50.0,
+                fontSize: 30.0,
                 fontWeight: FontWeight.w600,
                 fontFamily: AppStrings.conthraxFont,
               ),
--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -7,7 +7,7 @@ class AppStrings {
   static const resumeUrl =
       'https://drive.google.com/file/d/1pYajtWF84hB1cCZctinTaDXJ6z3Bkf3y/view?usp=sharing';
   static const lookingForward =
-      'Looking forward to building great projects with you.';
+      'Looking forward to building \n great projects with you.';
   static const aboutMe =
       "Hello, my name is Tamunotonye. I'm a mobile developer with over 3 years' experience using Flutter, I have made use of a verity of state management solutions with flutter like, Provider, Riverpod, and BloC, I've worked with firebase and custom backends for various projects. Catch me outside from work you will find gaming, at the gym working out or adventuring.";
   static const description =
